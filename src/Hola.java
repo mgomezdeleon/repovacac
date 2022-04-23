098b0778fc890170eda8d1347657a8118c393635
Marta <mgomezdeleon@ceinmark.net>
2022-04-23 23:32:05 +0200

    modificado por git

--- src/Hola.java
+++ src/Hola.java
@@ -2,5 +2,6 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Ceinmark");
 		System.out.println("Hola, soy workspace1");
+		System.out.println("Hola, soy git");
 		}
 }
