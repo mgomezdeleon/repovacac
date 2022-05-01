04f7a2b6c343464e3acbd66a831a8e7b2e4b6c08
Marta <mgomezdeleon@ceinmark.net>
2022-05-01 08:45:58 +0200

    Modificado por Rama2

--- src/Hola.java
+++ src/Hola.java
@@ -4,5 +4,6 @@ public class Hola {
 		System.out.println("Hola, soy workspace1");
 		System.out.println("Hola, soy git");
 		System.out.println("Hola, soy Rama1");
+		System.out.println("Hola, soy Rama2");
 		}
 }
